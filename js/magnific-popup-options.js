98cc7bd199112eb81b78a866ff158477798b54a5
Vestell <78303188+vestell@users.noreply.github.com>
2022-03-11 12:09:51 +0300

    Add files via upload

---
+++ js/magnific-popup-options.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".image-popup").magnificPopup({type:"image",removalDelay:300,mainClass:"mfp-with-zoom",gallery:{enabled:!0}}),$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({disableOn:700,type:"iframe",mainClass:"mfp-fade",removalDelay:160,preloader:!1,fixedContentPos:!1})}));
